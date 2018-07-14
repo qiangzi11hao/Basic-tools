b0d157f92ab3db4dfd9a4c731f007502e6a3e39b
qiangzi11hao <962960462@qq.com>
2018-07-15 01:29:28 +0800

    add bilstm

---
+++ BILSTM.py
@@ -0,0 +1,62 @@
+import tensorflow as tf
+
+
+class BILSTM(object):
+    def __init__(self, hidden_size, steps, num_words,num_layers=1, dropout=0, embedding_dim=100,
+                 is_training=False, embedding_matrix=None, final_unit=False):
+        self.hidden_size = hidden_size
+        self.num_steps = steps
+        self.num_words = num_words
+        self.num_layers = num_layers
+        self.dropout = dropout
+        self.embedding_dim = embedding_dim
+        self.final_unit = final_unit
+
+        self.input_x = tf.placeholder(tf.float32, [None, self.num_steps], name='input_x')
+
+        if embedding_matrix != None:
+            self.embedding = tf.Variable(embedding_matrix,
+                                         trainable = False,
+                                         name = 'embedding matrix',
+                                         dtype = tf.float32)
+        else:
+            self.embedding = tf.get_variable('embedding matrix',
+                                             shape = [self.num_words, self.embedding_dim],
+                                             dtype = tf.float32)
+
+        self.input_x_embed = tf.nn.embedding_lookup(self.embedding, self.input_x)
+
+        self.input_x_embed = tf.transpose(self.input_x_embed, [1, 0, 2])
+        self.input_x_embed = tf.unstack(self.input_x_embed)
+
+        lstm_fw_cell = tf.nn.rnn_cell.BasicLSTMCell(self.hidden_size)
+        lstm_bw_cell = tf.nn.rnn_cell.BasicLSTMCell(self.hidden_size)
+
+        if is_training:
+            lstm_fw_cell = tf.nn.rnn_cell.DropoutWrapper(lstm_fw_cell, output_keep_prob=(1 - self.dropout))
+            lstm_bw_cell = tf.nn.rnn_cell.DropoutWrapper(lstm_bw_cell, output_keep_prob=(1 - self.dropout))
+
+        lstm_fw_cell = tf.nn.rnn_cell.MultiRNNCell([lstm_fw_cell] * self.num_layers)
+        lstm_bw_cell = tf.nn.rnn_cell.MultiRNNCell([lstm_bw_cell] * self.num_layers)
+
+        self.length = tf.reduce_sum(tf.sign(self.input_x), reduction_indices=1)
+        self.length = tf.cast(self.length, tf.int32)
+
+        try:
+            self.outputs, _, _ = tf.nn.bidirectional_dynamic_rnn(lstm_fw_cell,
+                                                                 lstm_bw_cell,
+                                                                 self.input_x_embed,
+                                                                 dtype=tf.float32,
+                                                                 sequence_length=self.length)
+        except Exception:
+            self.outputs = tf.nn.bidirectional_dynamic_rnn(lstm_fw_cell,
+                                                                 lstm_bw_cell,
+                                                                 self.input_x_embed,
+                                                                 dtype=tf.float32,
+                                                                 sequence_length=self.length)
+        if self.final_unit:
+            self.outputs = self.outputs[-1]
+        else:
+            self.outputs = tf.stack(self.outputs)
+            self.outputs = tf.transpose(self.outputs, [1, 0, 2])
+
